7da8354f50d968e312d43dc2ff3232a7de1a2348
Chandana Challa <chch7974@colorado.edu>
2023-10-29 17:48:39 -0600

    Completed A8 changes

---
+++ base_external/package/aesdchar_driver/aesdchar_driver.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# AESDCHAR_DRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 8 git contents
+AESDCHAR_DRIVER_VERSION = '77fa2c707df5492b0bfed3c4de1bd443bae36535'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-chchbuff.git'
+AESDCHAR_DRIVER_SITE_METHOD = git
+AESDCHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESDCHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
